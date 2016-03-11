722ec47c1af46d06628a0b6da43e90741864febf
Christophe Debruel <krike06@gmail.com>
2016-03-11 11:55:05 +0100

    add service worker test code - append '-save-data' to feed smaller images

---
+++ service-workers-save-data/service-worker.js
@@ -0,0 +1,24 @@
+"use strict";
+
+this.addEventListener('fetch', event => {
+
+// Save Data support
+    if(event.request.headers.get('save-data')){
+
+    // We want to save data, so restrict icons and fonts too
+    if (event.request.url.includes('fonts.googleapis.com')) {
+        // return nothing
+        event.respondWith(new Response('', {status: 408, statusText: 'Ignore fonts to save data.' }));
+    }
+
+    if (/\.jpg$|.gif$|.png$/.test(event.request.url)) {
+        var saveDataUrl = event.request.url.substr(0, event.request.url.lastIndexOf(".")) + '-save-data' + event.request.url.substr(event.request.url.lastIndexOf("."), event.request.url.length - 1);
+        event.respondWith(
+            fetch(saveDataUrl, {
+                mode: 'no-cors'
+            })
+        );
+    }
+}
+
+});
